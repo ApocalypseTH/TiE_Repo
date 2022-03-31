4c24f5a8d87b0c62cefc6dce9570792915d79cd2
ApocalypseTH <36929115+ApocalypseTH@users.noreply.github.com>
2022-03-31 19:11:29 +0200

    fixed transfer, incapsulated abstract contracts

--- TiE_Tkn.sol
+++ TiE_Tkn.sol
@@ -1,9 +1,7 @@
-//known errors: / emit log for burn and mint 
 // SPDX-License-Identifier: MIT
-//made with love by InvaderTeam <3 :V:
+//made with love by InvaderTeam <3 :V: 34
 pragma solidity ^0.8.13;
-
-interface ERC20 {
+interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
@@ -14,8 +12,6 @@ interface ERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Burn(address indexed burner, uint256 value);
     event Mint(address indexed minter, uint256 value);
-    event OwnershipRelocated(address indexed previousOwner, address indexed newOwner);
-
 }
 
 library SafeMath {
@@ -31,73 +27,106 @@ library SafeMath {
     function mod(uint256 a, uint256 b) internal pure returns (uint256) { require(b > 0, "SafeMath: modulo by zero"); return a % b; }
 }
 
-contract Tie32 is ERC20 {
-    using SafeMath for uint256;    
-    mapping(address => uint) private _balances;
-    mapping(address => mapping(address => uint)) private _allowances;
-    mapping(address => bool) private _sellers;
-    mapping(address => bool) private _whiteList;
-    mapping(address => bool) private _blackList;
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {return msg.sender; } 
+    function _msgData() internal view virtual returns (bytes calldata) { return msg.data; }
+}
 
-    string constant private _name = "TieToken 32";
-    string constant private _symbol = "TIE32";
-    uint256 private  _supply = 50000 * (10 ** 6);
-    uint8 constant private _decimals = 6;
+abstract contract Ownable is Context {
     address private _owner;
-    bool private _reentrant_stat = false;
-
-    function _msgSender() private view returns (address) {return msg.sender; } //since they changed this bitch already countless times, better keep it in one place for easier update
-    //also, we better consider making getters and ownables in a separated contract since if not we need to declare functions in shitty places like this
-
-    modifier ownerRestricted { require(_msgSender() == _owner); _; }
-    modifier noreentrancy { require(!_reentrant_stat, "ReentrancyGuard: hijack detected"); _reentrant_stat = true; _; _reentrant_stat = false; }
     
-    constructor() {
-        _owner = 0x196B124DB02d9879BC05371Bd094b84e6d426151;
-        _balances[_owner] = _supply;
-        emit Transfer(address(this), _owner, _supply); //we can delete this, seemed nice for debug purposes
-        emit OwnershipRelocated(address(this), _owner);
+    event OwnershipRelocated(address indexed previousOwner, address indexed newOwner);
+
+    constructor () {
+        _owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+        emit OwnershipRelocated(address(0), _owner);
     }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier ownerRestricted { require(_owner == _msgSender()); _; }
+
+    function renounceOwnership() external ownerRestricted {
+        emit OwnershipRelocated(_owner, address(0));
+        _owner = address(0);
+    }
+
     function RelocateOwnership(address newOwner) external ownerRestricted {
-        require(newOwner != address(0), "Ownable_Danger: new owner is the zero address");
+        require(newOwner != address(0), "Ownable_Danger: new owner is the zero address"); 
         _owner = newOwner;
         emit OwnershipRelocated(_owner, newOwner);
     }
+}  
 
-    function getOwner() public view returns(address) { return _owner; }
+abstract contract Lists is Ownable {
+    //mapping(address => bool) private _whiteList; //maybe use this as a list of addresses to allow as _owner? seems a breach to me (Sam)
+    mapping(address => bool) private _blackList;
+    function WhiteList(address user) public ownerRestricted {
+        require(_blackList[user], "user not blacklisted");
+        _blackList[user] = false;
+    }
+    function BlackList(address user) public ownerRestricted {
+        require(!_blackList[user], "user already blacklisted");
+        _blackList[user] = true;
+    }
+    function BlackListed(address user) internal view returns (bool) {
+        return _blackList[user];
+    }
+}
+
+contract Tie35 is IERC20, Lists {
+    using SafeMath for uint256;    
+    mapping(address => uint) private _balances;
+    mapping(address => mapping(address => uint)) private _allowances;
+    //mapping(address => bool) private _sellers; //we need this?
+
+    string constant private _name = "TieToken 35";
+    string constant private _symbol = "TIE35";
+    uint256 private  _supply = 50000 * (10 ** 6);
+    uint8 constant private _decimals = 6;
+    bool private _reentrant_stat = false;
+
+    modifier noreentrancy { require(!_reentrant_stat, "ReentrancyGuard: hijack detected"); _reentrant_stat = true; _; _reentrant_stat = false; }
+    
+    constructor() {
+        _balances[owner()] = _supply;
+//        emit Transfer(address(this), owner(), _supply); //we can delete this, seemed nice for debug purposes
+    }
+
     function name() external pure returns(string memory) { return _name; }
     function symbol() external pure returns(string memory) { return _symbol; }
     function decimals() external pure returns(uint8) { return _decimals; }
-    function totalSupply() external view override returns(uint256) { return _supply.div( 10 ** _decimals); }       
-    function balanceOf(address wallet) external view override returns(uint256) { return _balances[wallet]; } //need to check if blacklisted, then return backup balance since the original is redistributed imo dunno damn this shit hard af xd
-    
+    function totalSupply() external view override returns(uint256) { return _supply.div(10 ** _decimals); }       
+    function balanceOf(address wallet) external view override returns(uint256) { return _balances[wallet]; } 
+//need to check if blacklisted, then return backup balance since the original is redistributed imo dunno damn this shit hard af xd
     function subSupply(uint256 amount) private { _supply = _supply.sub(amount); }
     function addSupply(uint256 amount) private { _supply = _supply.add(amount); }
-
 
     function beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {
         require(_balances[from] >= amount, "Insufficient funds.");
         require(from != address(0), "ERC20: approve from the zero address");
-        require(!_blackList[to], "Recipient is backlisted");
+        require(!BlackListed(to), "Recipient is blacklisted");
         require(to != address(0), "ERC20: burn from the zero address");
         require(amount > 0, "Empty transactions consume gas as well you moron");
     }
     function afterTokenTransfer(address to, uint256 amount) internal virtual { 
     }
 
-    function _approve(address keeper, address spender, uint256 amount) private { //couldn`t write owner instead of keeper, we better change that either or keep it like this
-        require(keeper != address(0), "ERC20: approve from the zero address");
+    function _approve(address owner, address spender, uint256 amount) private {
+        require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[keeper][spender] = amount;
-        emit Approval(keeper, spender, amount);
+        //require(!_blackList[owner], "Recipient is backlisted");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
     }
 
-    function _transfer(address from, address to, uint256 amount) internal noreentrancy {
+    function _transfer(address from, address to, uint256 amount) private noreentrancy {
         beforeTokenTransfer(from, to, amount);
-        _balances[_msgSender()] -= amount;
+        _balances[from] -= amount;
         _balances[to] += amount;      
-        emit Transfer(_msgSender(), to, amount);
+        emit Transfer(from, to, amount);
         afterTokenTransfer(to, amount);
     }
 
@@ -107,18 +136,19 @@ contract Tie32 is ERC20 {
     }
 
     function transferFrom(address from, address to, uint256 amount) external override returns(bool) {
+        beforeTokenTransfer(from, to, amount);
         _transfer(from, to, amount);
         _approve(from, _msgSender(), _allowances[from][_msgSender()]-amount);
         return true;
     }
-
+    
     function approve(address spender, uint256 amount) external override returns (bool) {
         _approve(_msgSender(), spender, amount);
         return true;
     }
 
-    function allowance(address fundsOwner, address spender) external view override returns (uint256) {
-        return _allowances[fundsOwner][spender];
+    function allowance(address owner, address spender) external view override returns (uint256) {
+        return _allowances[owner][spender];
     }
 
     function increaseAllowance(address spender, uint256 addValue) external virtual returns (bool) {
@@ -129,16 +159,6 @@ contract Tie32 is ERC20 {
     function decreaseAllowance(address spender, uint256 subValue) external virtual returns (bool) {
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender]-subValue);
         return true;
-    }
-
-    function whitelist(address user) public ownerRestricted {
-        require(_blackList[user], "user already whitelisted");
-        _blackList[user] = false;
-    }
-
-    function blackList(address user) public ownerRestricted {
-        require(!_blackList[user], "user already blacklisted");
-        _blackList[user] = true;
     }
 
     function mint(address account, uint256 amount) external ownerRestricted {
